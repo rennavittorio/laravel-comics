3610fc25c18cc31c3d60fb906709db0fc8ad5f91
rennavittorio <renna.vittorio@gmail.com>
2023-04-05 15:29:12 +0200

    add: add header navbar with some basic style applied

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    some home page content here..
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+<header class="header py-3 bg-secondary">
+    
+    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <ul class="list__link d-flex justify-content-end align-items-center gap-3 m-0 text-white">
+                    <li class="item__link">
+                        <a href="#">
+                            home
+                        </a>
+                    </li>
+                    <li class="item__link">
+                        <a href="#">
+                            about us
+                        </a>
+                    </li>
+                    <li class="item__link">
+                        <a href="#">
+                            contacts
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+</header>
